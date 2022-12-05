86d05d815f5df17b08f51a73cac30b76ec2c7e6c
Chen Gu <c.gu.1989@outlook.com>
2022-12-05 18:16:29 +0800

    Update smart-contract.sol

--- smart-contract.sol
+++ smart-contract.sol
@@ -1,24 +1,30 @@
 pragma solidity ^0.4.0;
 
-contract OnoffDetect{
-    uint256 thr = 2.0*1000;
-    uint256 TimeSlice = 30; //time :s
-    function time(uint256 ta, uint256 tb) public view returns(bool){
+contract onoffdetect{
+    uint256 thr = 0.8*1000;
+    uint256 timeslice = 30; //time :s
+
+    function time(uint256 ta, uint256 tb) public view returns(bool){ //时间片
         uint256 t;
         t = tb - ta;
-        if (t >= TimeSlice){
+        if (t >= timeslice){
             return true;
-        }        
+        }
+            
         else{
             return false;
         }
     }
-    function judge(uint256 df) public view returns(bool){
+
+    function judge(uint256 df) public view returns(bool) { //积分判断
         if (df > thr){
             return true;
-        }           
+        }
+            
         else{
             return false;
-        }         
+        }
+           
     }
+
 }
